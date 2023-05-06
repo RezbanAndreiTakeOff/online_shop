f46ea0c89764f9644edf65e1422642ba1c559aa2
RezbanAndreiTakeOff <andrei.rezban@takeofflabs.com>
2023-05-06 09:35:34 +0300

    add user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'extra#new'
 
   get 'goodbye', to: 'application#goodbye'
   get 'extra', to: 'application#extra'
-  get "sign_up", to: 'users#new'
+  get 'log_out', to: 'extra#new'
 end
