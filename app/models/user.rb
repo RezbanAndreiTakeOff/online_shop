40590a572fd650aa92bbd91f83def4978a1b70bf
RezbanAndreiTakeOff <andrei.rezban@takeofflabs.com>
2023-05-27 12:10:45 +0300

    create orders

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one :shipping_address
   has_one :billing_address
+  has_many :orders
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
