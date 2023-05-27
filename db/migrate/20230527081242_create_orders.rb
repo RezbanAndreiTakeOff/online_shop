2bbace6b50dac58c71156d190638a06a433c7353
RezbanAndreiTakeOff <andrei.rezban@takeofflabs.com>
2023-05-27 12:10:45 +0300

    create orders

---
+++ db/migrate/20230527081242_create_orders.rb
@@ -0,0 +1,12 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      t.bigint :product_id, null: false
+      t.bigint :user_id, null: false
+      t.integer :quantity, default: 1
+      t.string :tracking_number
+      t.string :status, default: "not shipped"
+      t.timestamps
+    end
+  end
+end
